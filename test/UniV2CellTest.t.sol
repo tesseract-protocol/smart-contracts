ead4f19666d5a41f276d62b09d2818e7aabb0abf
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2024-11-18 15:30:20 +0100

    add UniV2Cell

---
+++ test/UniV2CellTest.t.sol
@@ -0,0 +1,482 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "./BaseTest.t.sol";
+import "./../src/UniV2Cell.sol";
+
+contract UniV2CellTest is BaseTest {
+    address public UNIV2_FACTORY = 0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10;
+
+    function test_ERC20_ERC20_SwapAndTransfer() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        uint256 amountIn = 100e18;
+        UniV2Cell.Extras memory extras = UniV2Cell.Extras({slippageBips: 200});
+        (bytes memory trade, uint256 gasEstimate) = cell.route(amountIn, WAVAX, USDC, abi.encode(extras));
+        UniV2Cell.Trade memory decodedTrade = abi.decode(trade, (UniV2Cell.Trade));
+
+        vm.assertGt(decodedTrade.amountOut, 0);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.SwapAndTransfer,
+            requiredGasLimit: gasEstimate + 450_000,
+            recipientGasLimit: gasEstimate,
+            trade: trade,
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: false,
+                bridgeSourceChain: address(0),
+                bridgeDestinationChain: address(0),
+                cellDestinationChain: address(0),
+                destinationBlockchainID: "",
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        mockReceiveTokens(address(cell), address(wavaxTokenHome), amountIn, payload);
+
+        vm.assertApproxEqRel(IERC20(USDC).balanceOf(vm.addr(123)), decodedTrade.amountOut, 0.1e18);
+    }
+
+    function test_Native_ERC20_SwapAndTransfer() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        uint256 amountIn = 10e18;
+        UniV2Cell.Extras memory extras = UniV2Cell.Extras({slippageBips: 200});
+        (bytes memory trade, uint256 gasEstimate) = cell.route(amountIn, WAVAX, USDC, abi.encode(extras));
+        UniV2Cell.Trade memory decodedTrade = abi.decode(trade, (UniV2Cell.Trade));
+
+        vm.assertGt(decodedTrade.amountOut, 0);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.SwapAndTransfer,
+            requiredGasLimit: gasEstimate + 450_000,
+            recipientGasLimit: gasEstimate,
+            trade: trade,
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: false,
+                bridgeSourceChain: address(0),
+                bridgeDestinationChain: address(0),
+                cellDestinationChain: address(0),
+                destinationBlockchainID: "",
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        mockReceiveNative(address(cell), amountIn, payload);
+
+        vm.assertApproxEqRel(IERC20(USDC).balanceOf(vm.addr(123)), decodedTrade.amountOut, 0.1e18);
+    }
+
+    function test_ERC20_Native_SwapAndTransfer() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        uint256 amountIn = 1000e6;
+        UniV2Cell.Extras memory extras = UniV2Cell.Extras({slippageBips: 200});
+        (bytes memory trade, uint256 gasEstimate) = cell.route(amountIn, USDC, WAVAX, abi.encode(extras));
+        UniV2Cell.Trade memory decodedTrade = abi.decode(trade, (UniV2Cell.Trade));
+
+        vm.assertGt(decodedTrade.amountOut, 0);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.SwapAndTransfer,
+            requiredGasLimit: gasEstimate + 450_000,
+            recipientGasLimit: gasEstimate,
+            trade: trade,
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: false,
+                bridgeSourceChain: address(0),
+                bridgeDestinationChain: address(0),
+                cellDestinationChain: address(0),
+                destinationBlockchainID: "",
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        mockReceiveTokens(address(cell), address(usdcTokenHome), amountIn, payload);
+
+        vm.assertApproxEqRel(address(vm.addr(123)).balance, decodedTrade.amountOut, 0.1e18);
+    }
+
+    function test_ERC20_ERC20_Hop() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.Hop,
+            requiredGasLimit: 900_000,
+            recipientGasLimit: 450_000,
+            trade: "",
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: false,
+                bridgeSourceChain: address(usdcTokenHome),
+                bridgeDestinationChain: randomRemoteAddress,
+                cellDestinationChain: vm.addr(9876),
+                destinationBlockchainID: REMOTE_BLOCKCHAIN_ID,
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        vm.expectEmit(teleporterRegistry.getLatestTeleporter());
+        emit SendCrossChainMessage();
+        mockReceiveTokens(address(cell), address(usdcTokenHome), 1000e6, payload);
+    }
+
+    function test_ERC20_Native_Hop() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.Hop,
+            requiredGasLimit: 900_000,
+            recipientGasLimit: 450_000,
+            trade: "",
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: true,
+                bridgeSourceChain: address(nativeTokenHome),
+                bridgeDestinationChain: randomRemoteAddress,
+                cellDestinationChain: vm.addr(9876),
+                destinationBlockchainID: REMOTE_BLOCKCHAIN_ID,
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        vm.expectEmit(teleporterRegistry.getLatestTeleporter());
+        emit SendCrossChainMessage();
+        mockReceiveTokens(address(cell), address(wavaxTokenHome), 10e18, payload);
+    }
+
+    function test_Native_ERC20_Hop() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.Hop,
+            requiredGasLimit: 900_000,
+            recipientGasLimit: 450_000,
+            trade: "",
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: false,
+                bridgeSourceChain: address(wavaxTokenHome),
+                bridgeDestinationChain: randomRemoteAddress,
+                cellDestinationChain: vm.addr(9876),
+                destinationBlockchainID: REMOTE_BLOCKCHAIN_ID,
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        vm.expectEmit(teleporterRegistry.getLatestTeleporter());
+        emit SendCrossChainMessage();
+        mockReceiveNative(address(cell), 10e18, payload);
+    }
+
+    function test_ERC20_ERC20_HopAndCall() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.HopAndCall,
+            requiredGasLimit: 900_000,
+            recipientGasLimit: 450_000,
+            trade: "",
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: false,
+                bridgeSourceChain: address(wavaxTokenHome),
+                bridgeDestinationChain: randomRemoteAddress,
+                cellDestinationChain: vm.addr(9876),
+                destinationBlockchainID: REMOTE_BLOCKCHAIN_ID,
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        vm.expectEmit(teleporterRegistry.getLatestTeleporter());
+        emit SendCrossChainMessage();
+        mockReceiveTokens(address(cell), address(wavaxTokenHome), 100e18, payload);
+    }
+
+    function test_ERC20_Native_HopAndCall() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.HopAndCall,
+            requiredGasLimit: 900_000,
+            recipientGasLimit: 450_000,
+            trade: "",
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: true,
+                bridgeSourceChain: address(nativeTokenHome),
+                bridgeDestinationChain: randomRemoteAddress,
+                cellDestinationChain: vm.addr(9876),
+                destinationBlockchainID: REMOTE_BLOCKCHAIN_ID,
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        vm.expectEmit(teleporterRegistry.getLatestTeleporter());
+        emit SendCrossChainMessage();
+        mockReceiveTokens(address(cell), address(wavaxTokenHome), 100e18, payload);
+    }
+
+    function test_Native_ERC20_HopAndCall() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.HopAndCall,
+            requiredGasLimit: 900_000,
+            recipientGasLimit: 450_000,
+            trade: "",
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: false,
+                bridgeSourceChain: address(wavaxTokenHome),
+                bridgeDestinationChain: randomRemoteAddress,
+                cellDestinationChain: vm.addr(9876),
+                destinationBlockchainID: REMOTE_BLOCKCHAIN_ID,
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        vm.expectEmit(teleporterRegistry.getLatestTeleporter());
+        emit SendCrossChainMessage();
+        mockReceiveNative(address(cell), 10e18, payload);
+    }
+
+    function test_ERC20_ERC20_SwapAndHop() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        uint256 amountIn = 1000e6;
+        UniV2Cell.Extras memory extras = UniV2Cell.Extras({slippageBips: 200});
+        (bytes memory trade, uint256 gasEstimate) = cell.route(amountIn, USDC, WAVAX, abi.encode(extras));
+        UniV2Cell.Trade memory decodedTrade = abi.decode(trade, (UniV2Cell.Trade));
+
+        vm.assertGt(decodedTrade.amountOut, 0);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.SwapAndHop,
+            requiredGasLimit: gasEstimate + 450_000,
+            recipientGasLimit: gasEstimate,
+            trade: trade,
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: false,
+                bridgeSourceChain: address(wavaxTokenHome),
+                bridgeDestinationChain: randomRemoteAddress,
+                cellDestinationChain: vm.addr(9876),
+                destinationBlockchainID: REMOTE_BLOCKCHAIN_ID,
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        vm.expectEmit(teleporterRegistry.getLatestTeleporter());
+        emit SendCrossChainMessage();
+        mockReceiveTokens(address(cell), address(usdcTokenHome), amountIn, payload);
+    }
+
+    function test_ERC20_Native_SwapAndHop() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        uint256 amountIn = 1000e6;
+        UniV2Cell.Extras memory extras = UniV2Cell.Extras({slippageBips: 200});
+        (bytes memory trade, uint256 gasEstimate) = cell.route(amountIn, USDC, WAVAX, abi.encode(extras));
+        UniV2Cell.Trade memory decodedTrade = abi.decode(trade, (UniV2Cell.Trade));
+
+        vm.assertGt(decodedTrade.amountOut, 0);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.SwapAndHop,
+            requiredGasLimit: gasEstimate + 450_000,
+            recipientGasLimit: gasEstimate,
+            trade: trade,
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: true,
+                bridgeSourceChain: address(nativeTokenHome),
+                bridgeDestinationChain: randomRemoteAddress,
+                cellDestinationChain: vm.addr(9876),
+                destinationBlockchainID: REMOTE_BLOCKCHAIN_ID,
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        vm.expectEmit(teleporterRegistry.getLatestTeleporter());
+        emit SendCrossChainMessage();
+        mockReceiveTokens(address(cell), address(usdcTokenHome), amountIn, payload);
+    }
+
+    function test_Native_ERC20_SwapAndHop() public {
+        UniV2Cell cell = new UniV2Cell(vm.addr(1), WAVAX, UNIV2_FACTORY, 3, 120_000);
+
+        uint256 amountIn = 10e18;
+        UniV2Cell.Extras memory extras = UniV2Cell.Extras({slippageBips: 200});
+        (bytes memory trade, uint256 gasEstimate) = cell.route(amountIn, WAVAX, USDC, abi.encode(extras));
+        UniV2Cell.Trade memory decodedTrade = abi.decode(trade, (UniV2Cell.Trade));
+
+        vm.assertGt(decodedTrade.amountOut, 0);
+
+        Hop[] memory hops = new Hop[](1);
+        hops[0] = Hop({
+            action: Action.SwapAndHop,
+            requiredGasLimit: gasEstimate + 450_000,
+            recipientGasLimit: gasEstimate,
+            trade: trade,
+            bridgePath: BridgePath({
+                sourceBridgeIsNative: false,
+                bridgeSourceChain: address(usdcTokenHome),
+                bridgeDestinationChain: randomRemoteAddress,
+                cellDestinationChain: vm.addr(9876),
+                destinationBlockchainID: REMOTE_BLOCKCHAIN_ID,
+                teleporterFee: 0,
+                secondaryTeleporterFee: 0
+            })
+        });
+
+        Instructions memory instructions = Instructions({
+            rollbackTeleporterFee: 0,
+            rollbackGasLimit: 450_000,
+            receiver: vm.addr(123),
+            payableReceiver: true,
+            hops: hops
+        });
+
+        CellPayload memory payload =
+            CellPayload({instructions: instructions, sourceBlockchainID: "", rollbackDestination: address(0)});
+
+        vm.expectEmit(teleporterRegistry.getLatestTeleporter());
+        emit SendCrossChainMessage();
+        mockReceiveNative(address(cell), amountIn, payload);
+    }
+}
