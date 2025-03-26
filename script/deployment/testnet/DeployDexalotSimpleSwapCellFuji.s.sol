3d4d4c7448d76fd49c6e2d6e75edb7249900c4cf
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2025-03-26 13:47:34 +0100

    reorganize scripts

---
+++ script/deployment/testnet/DeployDexalotSimpleSwapCellFuji.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {TeleporterScriptBase} from "../../TeleporterScriptBase.sol";
+import {DexalotSimpleSwapCell} from "./../../../src/DexalotSimpleSwapCell.sol";
+
+// source .env && forge script script/deployment/testnet/DeployDexalotSimpleSwapCellFuji.s.sol:DeployDexalotSimpleSwapCellFuji --rpc-url $FUJI_RPC_URL --broadcast --skip-simulation --verifier-url 'https://api.routescan.io/v2/network/testnet/evm/43113/etherscan' --etherscan-api-key "verifyContract" --verify
+
+contract DeployDexalotSimpleSwapCellFuji is TeleporterScriptBase {
+    address constant MAINNET_RFQ = 0x1f06d7533890dBD67106Fee55FA9693D412e7551;
+    uint256 constant SWAP_GAS_ESTIMATE = 125_000;
+    address constant WRAPPED_NATIVE_TOKEN = 0xd00ae08403B9bbb9124bB305C09058E32C39A48c;
+    address public constant TELEPORTER_REGISTRY = 0xF86Cb19Ad8405AEFa7d09C778215D2Cb6eBfB228;
+    uint256 public constant MIN_TELEPORTER_VERSION = 1;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerAddress = vm.addr(deployerPrivateKey);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        new DexalotSimpleSwapCell(
+            deployerAddress,
+            WRAPPED_NATIVE_TOKEN,
+            TELEPORTER_REGISTRY,
+            MIN_TELEPORTER_VERSION,
+            MAINNET_RFQ,
+            SWAP_GAS_ESTIMATE
+        );
+
+        vm.stopBroadcast();
+    }
+}
